86c7910d719c2cdd4b55379e065b59a81b48337b
JuanManuelDorrego <juanmanueldorrego0@gmail.com>
2024-10-22 20:29:22 -0300

    segunda actualizacion

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+
+export default function SignInScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = () => {
+    console.log('Sign in with', email, password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign up</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#808080"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#808080"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+           <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        placeholderTextColor="#808080"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      
+
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Do you have an account?</Text>
+        <TouchableOpacity>
+          <Text style={styles.footerLink}>Sign in</Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  input: {
+    width: '100%',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 30,
+    marginBottom: 20,
+    color: '#333',
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    paddingVertical: 15,
+    paddingHorizontal: 80,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  footer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  footerText: {
+    color: '#808080',
+  },
+  footerLink: {
+    color: '#007BFF',
+    marginLeft: 5,
+  },
+});
